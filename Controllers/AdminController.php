7e7684247d44d603bc36bbf2e69d81d494ed95e2
Magali Fabri <magali_90@live.nl>
2022-03-24 21:27:59 +0100

    Refactor

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -6,18 +6,18 @@ class AdminController
     {
         // check for form
         if (!empty($_POST['title'])) {
-            $query = $_POST['title'];
-            $firstLetter = $query[0];
-            $apiUrl = "https://v2.sg.media-imdb.com/suggestion/{$firstLetter}/{$query}.json";
-            $searchResults = $this->getSearchResults($apiUrl);
-            // $searchResults = $_SESSION['data']; // dummy data
+            // $query = $_POST['title'];
+            // $firstLetter = $query[0];
+            // $apiUrl = "https://v2.sg.media-imdb.com/suggestion/{$firstLetter}/{$query}.json";
+            // $searchResults = $this->curlApi($apiUrl);
+            $searchResults = $_SESSION['data']; // dummy data
             $movies = $this->parseSearchResults($searchResults);
         }
 
         require_once 'Views/AdminView.php';
     }
 
-    private function getSearchResults($url): array
+    private function curlApi($url): array
     {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
@@ -56,18 +56,16 @@ class AdminController
         $apiKey = 'f2a26a58';
         $url = "http://www.omdbapi.com/?i={$imdbId}&apikey={$apiKey}";
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $responseJSON = curl_exec($curl);
-        curl_close($curl);
-        $data = json_decode($responseJSON, true);
-
-        // echo '<pre>';
-        // var_dump($data);
-        // echo '</pre>';
+        $data = $this->curlApi($url);
 
         // fill class with parsed data
+        $movie = $this->parseData($data, $imdbId);
+
+        require_once 'Views/MovieDetailsView.php';
+    }
+
+    private function parseData($data, $imdbId): Movie
+    {
         $title = $data['Title'];
         $year = $data['Year'];
         $genres = $data['Genre'];
@@ -79,6 +77,6 @@ class AdminController
         $movie->year = $year;
         $movie->genres = explode(', ', $genres);
 
-        require_once 'Views/MovieDetailsView.php';
+        return $movie;
     }
 }
