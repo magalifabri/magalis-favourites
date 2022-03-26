0d1d6b71c799d16e3bb0f0d4ab7204e9513628ec
Magali Fabri <magali_90@live.nl>
2022-03-26 14:05:33 +0100

    Make admin pages only accessible when logged in

--- Views/Movie/searchIndex.php
+++ Views/Movie/searchIndex.php
@@ -1,32 +1,37 @@
 <?php require_once 'Views/includes/header.php' ?>
 
 <main>
-    <!-- get movie title to search for  -->
+    <?php if (empty($_SESSION['loggedIn'])) : ?>
+        <p>permission denied</p>
+    <?php else : ?>
+        <!-- get movie title to search for  -->
 
-    <form action="" method="GET">
-        <input type="text" name="title" value="<?= $_GET['title'] ?? '' ?>">
-        <button type="submit" name="page" value="admin">search</button>
-    </form>
+        <form action="" method="GET">
+            <input type="text" name="title" value="<?= $_GET['title'] ?? '' ?>">
+            <button type="submit" name="page" value="admin">search</button>
+        </form>
 
-    <!-- load search results if they're available -->
+        <!-- load search results if they're available -->
 
-    <?php if (!empty($movies)) : ?>
-        <?php foreach ($movies as $movie) : ?>
+        <?php if (!empty($movies)) : ?>
+            <?php foreach ($movies as $movie) : ?>
 
-            <p>
-                <a href="?page=movie-details&imdb-id=<?= $movie->imdbId ?>">
-                    <?= $movie->title ?>
-                </a>
-            </p>
+                <p>
+                    <a href="?page=movie-details&imdb-id=<?= $movie->imdbId ?>">
+                        <?= $movie->title ?>
+                    </a>
+                </p>
 
-            <p><?= $movie->imdbId ?></p>
+                <p><?= $movie->imdbId ?></p>
 
-            <?php if (!empty($movie->imdbPosterUrl)) : ?>
-                <img height="200" src="<?= $movie->imdbPosterUrl ?>">
-            <?php endif ?>
+                <?php if (!empty($movie->imdbPosterUrl)) : ?>
+                    <img height="200" src="<?= $movie->imdbPosterUrl ?>">
+                <?php endif ?>
 
-        <?php endforeach ?>
+            <?php endforeach ?>
+        <?php endif ?>
     <?php endif ?>
+
 </main>
 
 <?php require_once 'Views/includes/footer.php' ?>
--- Views/Movie/searchShow.php
+++ Views/Movie/searchShow.php
@@ -1,21 +1,26 @@
 <?php require_once 'Views/includes/header.php' ?>
 
 <main>
-    <form action="?page=add-movie" method="POST">
-        <button type="submit" name="page" value="add-movie">+</button>
-        <input type="hidden" name="serialized-movie" value="<?= urlencode(serialize($movie)) ?>">
-    </form>
+    <?php if (empty($_SESSION['loggedIn'])) : ?>
+        <p>permission denied</p>
+    <?php else : ?>
+        <form action="?page=add-movie" method="POST">
+            <button type="submit" name="page" value="add-movie">+</button>
+            <input type="hidden" name="serialized-movie" value="<?= urlencode(serialize($movie)) ?>">
+        </form>
 
-    <p><?= $movie->title ?></p>
-    <p><?= $movie->year ?></p>
-    <p><?= $movie->imdbRating ?></p>
-    <img src="<?= $movie->imdbPosterUrl ?>" alt="">
-    <ul>
-        <?php foreach ($movie->genres as $genre) : ?>
-            <li><?= $genre ?></li>
-        <?php endforeach ?>
-    </ul>
-    <p><?= $movie->plot ?></p>
+        <p><?= $movie->title ?></p>
+        <p><?= $movie->year ?></p>
+        <p><?= $movie->imdbRating ?></p>
+        <img src="<?= $movie->imdbPosterUrl ?>" alt="">
+        <ul>
+            <?php foreach ($movie->genres as $genre) : ?>
+                <li><?= $genre ?></li>
+            <?php endforeach ?>
+        </ul>
+        <p><?= $movie->plot ?></p>
+    <?php endif ?>
+
 </main>
 
 <?php require_once 'Views/includes/footer.php' ?>
